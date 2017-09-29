e11925a13bcc3c467cbc34cffade100c97424954
rheza <rheza.satria.ta@gmail.com>
2017-09-29 23:59:52 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+## HSM PIN Block
+
+HSM PIN Block module. Provide PCI Standard PIN Block for banking, compliance with ISO-9564-format-1.
+
+
+## Usages
+
+### Generate PIN Block
+
+//TODO
+
+
+## License
+
+MIT (c) 2017
+
+## Need support?
+
+Contact author: Rheza Satria via Github message
---
+++ test/pin_block.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { encrypt, decrypt } = require('../lib/pin_block');
+
+describe('PIN Block ISO-9564-Format-1', () => {
+  const PIN = '111111';
+  const PAN = '6035159900000027';
+  const ZPK = 'ADD3B5C7B576D3AE38B90B7C0EB67A7C';
+
+  it('should encrypt PIN using des-ede-cbc with no-padding', () => {
+    const result = encrypt(ZPK, PIN, PAN);
+    assert.equal(result, '6619054d96892845');
+  });
+
+  it('should be able to decrypt PIN block', () => {
+    const pin = decrypt(ZPK, 'b069827e1b9a8de9');
+    assert.equal(pin, 'ae3761af02fb4e2f');
+  });
+});
